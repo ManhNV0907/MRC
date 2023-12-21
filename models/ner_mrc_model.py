7f94a1b5d9bd63449b45227ef701ee7298ebc84f
ManhNV0907 <manhqh0909@gmail.com>
2023-12-21 12:12:39 +0700

    [UPDATE]

--- models/ner_mrc_model.py
+++ models/ner_mrc_model.py
@@ -1,9 +1,33 @@
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 from torch import Tensor
 from typing import Tuple
 from transformers import BertModel
 
+class MultiNonLinearClassifier(nn.Module):
+    def __init__(self, hidden_size, num_label, dropout_rate=0.1, act_func="gelu", intermediate_hidden_size=1024):
+        super(MultiNonLinearClassifier, self).__init__()
+        self.num_label = num_label
+        self.intermediate_hidden_size = hidden_size if intermediate_hidden_size is None else intermediate_hidden_size
+        self.classifier1 = nn.Linear(hidden_size, self.intermediate_hidden_size)
+        self.classifier2 = nn.Linear(self.intermediate_hidden_size, self.num_label)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.act_func = act_func
+
+    def forward(self, input_features):
+        features_output1 = self.classifier1(input_features)
+        if self.act_func == "gelu":
+            features_output1 = F.gelu(features_output1)
+        elif self.act_func == "relu":
+            features_output1 = F.relu(features_output1)
+        elif self.act_func == "tanh":
+            features_output1 = F.tanh(features_output1)
+        else:
+            raise ValueError
+        features_output1 = self.dropout(features_output1)
+        features_output2 = self.classifier2(features_output1)
+        return features_output2
 
 class NERMRCModel(nn.Module):
     def __init__(self, context_presenter: BertModel, hidden_dim: int):
@@ -11,7 +35,9 @@ class NERMRCModel(nn.Module):
         self.context_presenter: BertModel = context_presenter
         self.start_classifier = nn.Linear(in_features=hidden_dim, out_features=1)
         self.end_classifier = nn.Linear(in_features=hidden_dim, out_features=1)
-        self.match_classifier = nn.Linear(in_features=2*hidden_dim, out_features=1)
+        # self.match_classifier = nn.Linear(in_features=2*hidden_dim, out_features=1)
+        self.match_classifier = MultiNonLinearClassifier(hidden_size=2*hidden_dim, num_label=1)
+
 
     def forward(self, input_ids: Tensor, token_type_ids: Tensor,
                 attention_mask: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
